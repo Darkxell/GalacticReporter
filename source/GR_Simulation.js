7027c453b20ae4ebc83db3090edf06551980eef4
CANDELA Nicolas <darkxell.mc@gmail.com>
2026-08-07 14:19:14 +0200

    Added Buffs to SimulatedShip

--- source/GR_Simulation.js
+++ source/GR_Simulation.js
@@ -26,18 +26,70 @@ export class SimulatedShip {
             itemcooldowns.push({ item: item, cooldown: 0 });
         }
         this.itemcooldowns = itemcooldowns;
+        // Array of buffs this ship has.
+        // Each buff has the following structure : {type, strength, duration, source}
+        // duration is in seconds, strength is a percentage
+        this.buffs = [];
+        // TODO : add drones and boosters as permanent buffs here
     }
 
     /** Advances time for this simulated ship by the given amount, in seconds. */
     tick(ticktime) {
         this.gcd -= ticktime;
         if (this.gcd < 0) this.gcd = 0;
-        for (var item of this.itemcooldowns) {
+        for (let item of this.itemcooldowns) {
             item.cooldown -= ticktime;
             if (item.cooldown < 0) item.cooldown = 0;
         }
+        for (let buff of this.buffs) {
+            // Buffs lasting more than 1hour are considered permanent
+            if(buff.duration <= 3600)buff.duration -= ticktime;
+        }
+        this.buffs = this.buffs.filter(b => b.duration <= 0);
     }
 
+    /** 
+    * Returns the total amount a stat of this ship is buffed by. 
+    *
+    * @param type Name of the buff type. Should be one of the following:
+    * [range, damage, accuracy, critical, speed, cooldown, healing, armor, drop, cryonite, energy, energycp]
+    *
+    * @returns the buff factor scalar. 1 if no buff apply, otherwise between 0 and the maximum buff amount.
+    */
+    getTotalBuffValue(type){
+        let scalar = 1;
+        for (let buff of this.buffs) {
+            if(buff.type !== type) continue;
+            switch (type) {
+                case "range":
+                case "damage":
+                case "accuracy":
+                case "critical":
+                case "speed":
+                case "healing":
+                case "drop":
+                case "energy":
+                case "energycap":
+                    // Additive buffs
+                    scalar += buff.strength / 100;
+                    break;
+                case "cryonite":
+                    // Multiplicative buffs
+                    scalar *= (100 + buff.strength) / 100;
+                    break;
+                case "cooldown":
+                case "armor":
+                    // Multiplicative reductions
+                    scalar *= (100 - buff.strength) / 100;
+                    break;
+                default:
+                    console.log("Unsupported buff type " + type + ", assumed this ship is not buffed.");
+                    return 1;
+            }
+        }
+        return scalar;
+    }
+    
     /** 
      * Asks this SimulatedShip instance to use the given item.
      * The item must be part of the 
